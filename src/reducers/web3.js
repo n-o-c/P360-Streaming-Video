e3b9a8a05fb05d17affe616bf011e19b2494fbb8
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-04-09 15:24:26 +0400

    Update web3.js

--- src/reducers/web3.js
+++ src/reducers/web3.js
@@ -1,21 +1,38 @@
-import { SET_WEB3 } from '../actions/types';
+import Web3 from 'web3';
+import CreditToken from '../contracts/CreditToken.json';
+import NFTSubscription from '../contracts/NFTSubscription.json';
+import RewardSystem from '../contracts/RewardSystem.json';
 
-const initialState = {
-  web3: null,
-  contracts: null,
+export const getWeb3 = async () => {
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    try {
+      await window.ethereum.enable();
+      return web3;
+    } catch (error) {
+      throw new Error('User denied account access');
+    }
+  } else if (window.web3) {
+    return new Web3(window.web3.currentProvider);
+  } else {
+    throw new Error('No web3 provider detected');
+  }
 };
 
-export default function (state = initialState, action) {
-  const { type, payload } = action;
-
-  switch (type) {
-    case SET_WEB3:
-      return {
-        ...state,
-        web3: payload.web3,
-        contracts: payload.contracts,
-      };
-    default:
-      return state;
-  }
-}
+export const getContracts = async web3 => {
+  const networkId = await web3.eth.net.getId();
+  const deployedNetwork = CreditToken.networks[networkId];
+  const creditToken = new web3.eth.Contract(
+    CreditToken.abi,
+    deployedNetwork && deployedNetwork.address
+  );
+  const nftSubscription = new web3.eth.Contract(
+    NFTSubscription.abi,
+    deployedNetwork && deployedNetwork.address
+  );
+  const rewardSystem = new web3.eth.Contract(
+    RewardSystem.abi,
+    deployedNetwork && deployedNetwork.address
+  );
+  return { creditToken, nftSubscription, rewardSystem };
+};
