2a2d6688ed60162e7be0b0edaf7bcc2bd570516c
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-04-09 15:05:25 +0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,43 @@ import { useSelector } from 'react-redux';
 import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
 
 const Navbar = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          P360
-        </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        {isAuthenticated && (
-          <>
-            <Button color="inherit" component
+return (
+<AppBar position="static">
+<Toolbar>
+<Typography variant="h6" style={{ flexGrow: 1 }}>
+P360
+</Typography>
+<Button color="inherit" component={Link} to="/">
+Home
+</Button>
+{isAuthenticated && (
+<>
+<Button color="inherit" component={Link} to="/dashboard">
+Dashboard
+</Button>
+<Button color="inherit" component={Link} to="/upload">
+Upload
+</Button>
+<Button color="inherit" component={Link} to="/search">
+Search
+</Button>
+</>
+)}
+{!isAuthenticated && (
+<>
+<Button color="inherit" component={Link} to="/register">
+Register
+</Button>
+<Button color="inherit" component={Link} to="/login">
+Login
+</Button>
+</>
+)}
+</Toolbar>
+</AppBar>
+);
+};
+
+export default Navbar;
