a11133a0c676af7a33452cff93a1c488d3af9e8e
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-04-09 12:38:44 +0400

    Create adminController.js

---
+++ server/controllers/adminController.js
@@ -0,0 +1,15 @@
+exports.getAllUsers = async (req, res, next) => {
+  // Implement logic to get all users from the database
+};
+
+exports.getUserById = async (req, res, next) => {
+  // Implement logic to get a specific user by ID from the database
+};
+
+exports.updateUser = async (req, res, next) => {
+  // Implement logic to update a specific user by ID in the database
+};
+
+exports.deleteUser = async (req, res, next) => {
+  // Implement logic to delete a specific user by ID from the database
+};
