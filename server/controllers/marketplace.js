fa2dfaf4597ba8c0d669deb5326f934b2ccacd46
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-04-09 12:35:52 +0400

    Delete server/controllers directory

--- server/controllers/marketplace.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const marketplaceController = require("../controllers/marketplaceController");
-
-router.get("/nfts", marketplaceController.getAllNFTs);
-router.get("/nft/:id", marketplaceController.getNFTById);
-router.post("/purchase", marketplaceController.purchaseNFT);
-
-module.exports = router;
