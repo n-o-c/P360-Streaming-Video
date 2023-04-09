3bc851eba4d3ae52b929bad09da88bb116313f95
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-04-09 15:04:27 +0400

    Create Navbar.js
    The main navigation bar of the application.

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+
+const Navbar = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          P360
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        {isAuthenticated && (
+          <>
+            <Button color="inherit" component
